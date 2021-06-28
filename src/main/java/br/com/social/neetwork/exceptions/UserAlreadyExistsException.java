a1dbf5d64f4cd6efe9039908815c37127f16eea6
Matheus <matheus.sys.dev@gmail.com>
2021-06-28 15:00:37 -0300

    first commit

--- src/main/java/br/com/social/neetwork/exceptions/UserAlreadyExistsException.java
+++ src/main/java/br/com/social/neetwork/exceptions/UserAlreadyExistsException.java
@@ -8,7 +8,7 @@ public class UserAlreadyExistsException extends RuntimeException {
 	private static final long serialVersionUID = -6832512384245550822L;
 	
 	public UserAlreadyExistsException() {
-		super("Usúario já existe.");
+		super("Usuário já existe!");
 	}
 
 }
